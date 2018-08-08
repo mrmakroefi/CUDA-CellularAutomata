71ab295eb20adfc7d7ca7f0c048ea714ecc11332
Muhammad Rizal Ma'rufi <mrm_int_main@yahoo.com>
2018-08-08 21:00:13 +0700

    Add files via upload

---
+++ Example/CUDA .cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace CUDACheck {
+    public static class CUDA {
+        [DllImport("DLL_CUDA")]
+        public static extern IntPtr getDeviceName();
+        
+        [DllImport("DLL_CUDA")]
+        public static extern void GPU_simulateCA(IntPtr data, ushort width, ushort height,byte chanceToAlive, byte starvationLimit, byte birthLimit, int seed, byte iterationsCount, ushort threadsCount);
+
+        [DllImport("DLL_CUDA")]
+        public static extern void CPU_initMap(IntPtr data, uint size, byte chanceToAlive, uint seed);
+
+        [DllImport("DLL_CUDA")]
+        public static extern IntPtr CPU_simulateCA(IntPtr input, ushort width, ushort height, byte chanceToAlive, byte starvationLimit, byte birthLimit, int seed, byte iterationsCount);
+        
+        [DllImport("DLL_CUDA")]
+        public static extern void CPU_floodFill(IntPtr data, uint width, uint height, uint wallThreshold, uint floorThreshold);
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CUDACheck {
+    class Program {
+        static void Main(string[] args)
+        {
+            IntPtr ptrName;
+            string devName;
+
+            ptrName = CUDA.getDeviceName();
+            devName = Marshal.PtrToStringAnsi(ptrName);
+            Console.WriteLine(devName);
+
+            byte iterations = 2;
+            ushort width = 512;
+            ushort height = 512;
+            uint length = (uint)(width * height);
+
+            Console.WriteLine("\n====== World Information ======");
+            Console.WriteLine("- World size: (" + width + "," + height + ")");
+            Console.WriteLine("- Total cell: " + length);
+            Console.WriteLine("===============================\n");
+
+            byte[] roll = new byte[length];
+            IntPtr testNum = Marshal.AllocHGlobal(roll.Length);
+            Marshal.Copy(roll, 0, testNum, roll.Length);
+            
+            CUDA.CPU_simulateCA(testNum, width, height, 35, 3, 3, 1, iterations);
+            //CUDA.GPU_simulateCA(testNum, width, height, 35, 3, 3, 1, iterations, 128);
+
+            CUDA.CPU_floodFill(testNum, width, height, 12, 8);
+
+            Marshal.Copy(testNum, roll, 0, roll.Length);
+            
+           // PrintData(roll, width, length);
+
+
+            Console.ReadKey();
+
+            Marshal.FreeHGlobal(testNum);
+            Marshal.FreeHGlobal(ptrName);
+        }
+
+        static void PrintData(byte[] data, ushort width, uint worldSize)
+        {
+            for (int i = 0; i < worldSize; i++)
+            {
+                Console.Write(data[i] + "");
+                if ((i + 1) % width == 0 && i != 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
